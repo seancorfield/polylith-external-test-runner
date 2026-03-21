9362ba73435eacc2ebb419162ec7ba162f6d61bf
Sean Corfield <sean@corfield.org>
2026-03-21 16:48:40 -0400

    [explore] move olical dep to project
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-for alias in "" ":olical"
+for config in source dev dummy
 do
-  for config in source dev dummy
-  do
-    clojure -M:poly$alias test :all with:$config
-    if test $? -ne 0
-    then
-      echo "Tests failed with :$config (alias: ${alias:-none})"
-      exit 1
-    fi
-  done
+  clojure -M:poly test :all with:$config
+  if test $? -ne 0
+  then
+    echo "Tests failed with :$config"
+    exit 1
+  fi
 done
